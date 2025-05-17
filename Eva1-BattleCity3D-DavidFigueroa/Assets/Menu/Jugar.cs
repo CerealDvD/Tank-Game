f644ef209dd2b97cfbbc2650aea68e5564a4cb14
David Figueroa <david.figueroa5@virginiogomez.cl>
2025-05-16 23:27:00 -0400

    Se codificaron los botones para poder enviar los respectivos datos a la pantallade juego

--- Eva1-BattleCity3D-DavidFigueroa/Assets/Menu/Jugar.cs
+++ Eva1-BattleCity3D-DavidFigueroa/Assets/Menu/Jugar.cs
@@ -2,23 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class Jugar : MonoBehaviour
 {
+    public TMP_InputField inputJugador;
+    public Slider sliderVolumen;
+
+    void GuardarPreferencias()
+    {
+        string nombre = inputJugador.text.Trim();
+        float volumen = sliderVolumen.value;
+
+        // Guarda nombre (puedes validar si está vacío)
+        if (string.IsNullOrEmpty(nombre))
+            nombre = "Jugador";
+
+        PlayerPrefs.SetString("NombreJugador", nombre);
+        PlayerPrefs.SetFloat("Volumen", volumen);
+    }
+
     public void JugarF()
     {
+        GuardarPreferencias();
         PlayerPrefs.SetInt("Dificultad", 1);
         SceneManager.LoadScene("Juego");
     }
 
     public void JugarM()
     {
+        GuardarPreferencias();
         PlayerPrefs.SetInt("Dificultad", 2);
         SceneManager.LoadScene("Juego");
     }
 
     public void JugarD()
     {
+        GuardarPreferencias();
         PlayerPrefs.SetInt("Dificultad", 3);
         SceneManager.LoadScene("Juego");
     }
